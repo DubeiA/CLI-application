fcb9bd0c06e481a8285f37840243c6255deefcfa
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-03-26 14:42:35 +0300

    done task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CLI-application
+
+Monosnap link - https://monosnap.com/list/64202af234d15cbc28f91bb0
--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.resolve('./db/contacts.json');
 
 function listContacts() {
   fs.readFile(contactsPath, (error, data) => {
@@ -18,31 +18,50 @@ function getContactById(contactId) {
       console.log(error);
     }
 
-    const visibleName = data.toString();
-    const obj = JSON.parse(visibleName);
-    const user = obj.filter((user) => user.id.includes(contactId));
-    console.log(user);
+    const contacts = data.toString();
+    const parseContacts = JSON.parse(contacts);
+    const findUser = parseContacts.filter(user => user.id.includes(contactId));
+    console.log(findUser);
+    return findUser;
   });
 }
 
 function removeContact(contactId) {
-  fs.unlink(contactsPath, (error, data) => {
+  fs.readFile(contactsPath, (error, data) => {
     if (error) {
       console.log(error);
     }
-    const visibleName = data.toString();
-    const obj = JSON.parse(visibleName);
-    const user = obj.filter((user) => user.id.includes(contactId));
-    console.log(user);
+
+    const contacts = data.toString();
+    const parseContacts = JSON.parse(contacts);
+    const findUser = parseContacts.filter(user => user.id !== contactId);
+    const stringifyUser = JSON.stringify(findUser);
+    fs.writeFile(contactsPath, stringifyUser, (error, data) => {
+      console.log(stringifyUser);
+    });
   });
 }
 
 function addContact(name, email, phone) {
-  fs.writeFile(contactsPath, [name, email, phone], (error, data) => {
+  fs.readFile(contactsPath, [name, email, phone], (error, data) => {
     if (error) {
       console.log(error);
     }
-    console.log(data);
+
+    const contacts = data.toString();
+    const parseContacts = JSON.parse(contacts);
+
+    const newContact = {
+      id: String(parseContacts.length + 1),
+      name,
+      email,
+      phone,
+    };
+    parseContacts.push(newContact);
+    const stringifyNewContact = JSON.stringify(parseContacts);
+    fs.writeFile(contactsPath, stringifyNewContact, (error, data) => {
+      console.log(stringifyNewContact);
+    });
   });
 }
 
